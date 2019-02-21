5df083c03a2d82d218fbca20d544bd47f0463641
david <s4dakaub@uni-trier.de>
2019-02-21 14:48:04 +0100

    test

--- src/main/java/com/javainuse/route/ReceiveMailRoute.java
+++ src/main/java/com/javainuse/route/ReceiveMailRoute.java
@@ -10,7 +10,7 @@ public class ReceiveMailRoute  extends RouteBuilder{
         public void configure() throws Exception {
             //from("imaps://imap.gmail.com:993?username=sudokusolver2019&password=#sudokuSolver2019")//"file:C:/inputFolder?move=./done"
             //from("imap://localhost:143?username=email2&password=apfelmusmann")//"file:C:/inputFolder?move=./done"
-            from("imap://192.168.178.42:143?username=email1&password=apfelmusmann&delay=5")//"file:C:/inputFolder?move=./done"
+            from("imap://136.199.13.190:143?username=email1&password=apfelmusmann&delay=5")//"file:C:/inputFolder?move=./done"
                     //&debugMode=true
                     .process(new MyProcessor())
                     .bean(new MyTransformer(), "TransformContext")
