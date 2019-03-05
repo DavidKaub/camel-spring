72975e15b2689f9a774dc93ae015e06ee4706bf1
thefeinkoster <s4dakaub@uni-trier.de>
2019-03-05 16:10:11 +0100

    changed structure: TODOS: Implement Different Routes (From Email alredy kind of exists / to email part can be copied to the following implementations: From Mqtt and From REST To Mail

---
+++ src/main/java/com/javainuse/bean/FromRestTransformer.java
@@ -0,0 +1,9 @@
+package com.javainuse.bean;
+
+public class FromRestTransformer {
+
+    private String transformRestToEmail(String body){
+        return null;
+    }
+
+}
--- src/main/java/com/javainuse/bean/MyTransformer.java
+++ src/main/java/com/javainuse/bean/MyTransformer.java
@@ -2,10 +2,7 @@ package com.javainuse.bean;
 
 public class MyTransformer {
 
-    public String TransformContext(String body){
-        String upper = body.toUpperCase();
-        return upper;
 
-    }
+
 
 }
