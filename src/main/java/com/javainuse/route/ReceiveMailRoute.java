f4847462b37a8ba923f7ba1735bffa57b9cb8f61
David <s4dakaub@uni-trier.de>
2019-03-10 18:06:48 +0100

    deleted from pom:
            <dependency>
                <groupId>org.slf4j</groupId>
                <artifactId>slf4j-api</artifactId>
                <version>2.17.6</version>
            </dependency>
    
            <dependency>
                <groupId>org.slf4j</groupId>
                <artifactId>slf4j-log4j12</artifactId>
                <version>2.17.6</version>
            </dependency>

--- src/main/java/com/javainuse/route/ReceiveMailRoute.java
+++ src/main/java/com/javainuse/route/ReceiveMailRoute.java
@@ -4,13 +4,13 @@ import com.javainuse.bean.MyTransformer;
 import com.javainuse.processor.MyProcessor;
 import org.apache.camel.builder.RouteBuilder;
 
-public class ReceiveMailRoute  extends RouteBuilder{
+public class ReceiveMailRoute extends RouteBuilder{
 
         @Override
         public void configure() throws Exception {
-            //from("imaps://imap.gmail.com:993?username=sudokusolver2019&password=#sudokuSolver2019")//"file:C:/inputFolder?move=./done"
+            from("imaps://imap.gmail.com:993?username=sudokusolver2019&password=#sudokuSolver2019")//"file:C:/inputFolder?move=./done"
             //from("imap://localhost:143?username=email2&password=apfelmusmann")//"file:C:/inputFolder?move=./done"
-            from("imap://192.168.178.42:143?username=email1&password=apfelmusmann&delay=5")//"file:C:/inputFolder?move=./done"
+            //from("imap://192.168.178.42:143?username=email1&password=apfelmusmann&delay=5")//"file:C:/inputFolder?move=./done"
                     //&debugMode=true
                     .process(new MyProcessor())
                     .bean(new MyTransformer(), "TransformContext")
